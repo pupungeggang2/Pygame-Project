e813ad7596b255c3b74bb97b4b7ebb238f62460e
pupungeggang2 <kyongkyong2892@gmail.com>
2024-06-22 03:12:53 +0900

    Start

--- Card/main.py
+++ Card/main.py
@@ -1,10 +1,25 @@
-import pygame
-import os
+import pygame, sys
+
+import var, const, asset, UI
+import scenetitle, scenefield, scenegame
 
 def init():
-    pass
+    var.screen = pygame.display.set_mode(var.resolution)
+    pygame.display.set_caption('Card')
+    var.clock = pygame.time.Clock()
 
 def main():
+    while True:
+        var.clock.tick(var.FPS)
+        handle_input()
+        handle_scene()
+
+def handle_input():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def handle_scene():
     pass
 
 if __name__ == '__main__':
