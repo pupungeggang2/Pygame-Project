9e74eace29c56e30959a21ba8bfd29c39ce4fd48
pupungeggang2 <kyongkyong2892@gmail.com>
2024-07-09 01:24:33 +0900

    Start

--- Card/Game/main.py
+++ Card/Game/main.py
@@ -3,7 +3,25 @@ import asset, UI, const, var
 import scenetitle, scenefield, scenegame
 
 def init():
-    pass
+    pygame.init()
+    var.screen = pygame.display.set_mode(var.resolution)
+    pygame.display.set_caption('Card')
+    var.clock = pygame.time.Clock()
 
 def main():
+    while True:
+        var.clock.tick(var.FPS)
+        handle_input()
+        handle_scene()
+        
+def handle_input():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def handle_scene():
     pass
+    
+if __name__ == '__main__':
+    init()
+    main()
---
+++ Card/Game/var.py
@@ -0,0 +1,4 @@
+screen = None
+resolution = [1280, 720]
+FPS = 60
+clock = None
