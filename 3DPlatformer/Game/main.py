d7123b44491b5d4a2c5303b6f69afdddb720cb83
pupungeggang2 <kyongkyong2892@gmail.com>
2024-08-06 11:19:42 +0900

    0.0.0

--- 3DPlatformer/Game/main.py
+++ 3DPlatformer/Game/main.py
@@ -1,31 +1,80 @@
-import sys, pygame
-import var, const
-
+import glfw
 from OpenGL.GL import *
-from OpenGL.GLU import *
-
-def init():
-    pygame.init()
-    var.screen = pygame.display.set_mode(var.resolution, pygame.SCALED|pygame.OPENGL)
-    var.screen_ui = pygame.display.set_mode(var.resolution, pygame.SCALED)
-    pygame.display.set_caption('GL Test')
-    var.clock = pygame.time.Clock()
-
+import OpenGL.GL.shaders
+import numpy
+ 
+ 
 def main():
-    while True:
-        var.clock.tick(var.FPS)
-    
-        handle_input()
-        var.screen.fill((0, 0, 0))
-        pygame.draw.rect(var.screen, (0, 255, 0), (0, 0, 40, 40))
-        pygame.display.flip()
-
-def handle_input():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-if __name__ == '__main__':
-    init()
+ 
+    # initialize glfw
+    if not glfw.init():
+        return
+ 
+    window = glfw.create_window(800, 600, "My OpenGL window", None, None)
+ 
+    if not window:
+        glfw.terminate()
+        return
+ 
+    glfw.make_context_current(window)
+    #            positions        colors
+    triangle = [-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+                 0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+                 0.0,  0.5, 0.0, 0.0, 0.0, 1.0]
+ 
+    triangle = numpy.array(triangle, dtype = numpy.float32)
+ 
+    vertex_shader = """
+    #version 330
+    in vec3 position;
+    in vec3 color;
+    out vec3 newColor;
+    void main()
+    {
+        gl_Position = vec4(position, 1.0f);
+        newColor = color;
+    }
+    """
+ 
+    fragment_shader = """
+    #version 330
+    in vec3 newColor;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(newColor, 1.0f);
+    }
+    """
+    shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
+                                              OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
+ 
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, 72, triangle, GL_STATIC_DRAW)
+ 
+    position = glGetAttribLocation(shader, "position")
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(position)
+ 
+    color = glGetAttribLocation(shader, "color")
+    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(color)
+ 
+ 
+    glUseProgram(shader)
+ 
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+ 
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+ 
+        glClear(GL_COLOR_BUFFER_BIT)
+ 
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+ 
+        glfw.swap_buffers(window)
+ 
+    glfw.terminate()
+ 
+if __name__ == "__main__":
     main()
